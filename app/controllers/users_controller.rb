36a717243f293e1ff060be8786981c845d5faf66
Le Tri Thanh <letrithanh@outlook.com>
2022-10-08 11:43:29 +0000

    add error messages when signup errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      flash[:create_user_errors] = []
+      @user.errors.full_messages.each { |msg| flash[:create_user_errors] << msg }  
+      redirect_to new_user_path # TODO: investigate why can't use render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'signup' => 'users#new'
+
+  resources :users
 end
