7fc6783ed324094184a6c0df0997c48b15480f40
Le Tri Thanh <letrithanh@outlook.com>
2022-10-09 10:47:12 +0000

    remove cover_url col of novel and volume table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_09_041625) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_09_104529) do
   create_table "chapters", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -22,7 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_041625) do
 
   create_table "novels", force: :cascade do |t|
     t.string "name"
-    t.string "cover_url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -38,7 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_041625) do
 
   create_table "volumes", force: :cascade do |t|
     t.string "name"
-    t.string "cover_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "novels_id", null: false
